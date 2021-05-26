012a9c0430af208a80809e43293023c917122904
Erik Bergland <54935266+Bergan27@users.noreply.github.com>
2021-05-26 14:13:35 +0200

    småfix

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,11 +27,9 @@ namespace BattleShips
         }
         private void Coordinates_X(object sender, RoutedEventArgs e)
         {
- 
         }
         private void Coordinates_1(object sender, RoutedEventArgs e)
         {
-
         }
         public void GameBoard_Loaded(object sender, RoutedEventArgs e)
         {
@@ -57,48 +55,37 @@ namespace BattleShips
                     {
                         button.Content = 0;
                     }
-
                     // Jag hade tänkt här att göra en switch av något slag som tog in värden från de olika knapparna och sparade dem i 2 olika listor
                     // En för spelare 1 och en för spelare 2 som sparat alla skepp som sätts ut med ett värde på 1 och vatten värde 0
                     // när man väl träffat ett motståndar skepp så hade värdet på den knappen säts om till o 
                     // Då hade en annan funktion kollat av om listans värde var lika med 0 och skrivit ut att den spelare som sänkt alla skepp vunnnit
-                    // 
                 });
                 Grid.SetRow(button, i);
                 Grid.SetColumn(button, j);
                 GameBoard.Children.Add(button);
-                    
-
                 }
             }
-            
         }
-
         public void Game()
-          
         {
-
             // Det skulle funnits en funktion som tagit "block" som skulle varit de olika skeppen och satt ett värde på 1-4 knappar i rad
             // Skeppen skulle ärva funktionalitet av varandra och öka storleken med 1 
             // Det skulle funnits en gräns för skeppen så att de inte skulle kunna sättas utanför spelplanen 
             // Och skeppen skulle kunna roteras med hjälp av horisontalalignment / verticalalignment
             var turn = 0;
-
             var player1Turns = true;
             while (true)
             {
                 turn++;
                 if (player1Turns)
                 {
-                    //player one plays
+                    //spelare 1 spelare
                 }
                 else
                 {
                     // Här är en enkel funktion som roterar omgångarna mellan spelare ett och två
-                    // Input från denna hade tagits för att bestämma vilken lista som input från knapparna skulle sparats i 
-                    
-                    
-                    //player two plays
+                    // Input från denna loop hade tagits för att bestämma vilken lista som input från knapparna skulle sparats i 
+                    //spelare 2 spelar
                 }
                 if (turn % 3 == 0)
                 {
@@ -106,13 +93,10 @@ namespace BattleShips
                 }
             }
         }
-
-
-
-        int[,] buttonListPlayer1 = new int[10, 10];
-        var Row = Grid.GetRow(button);
-        var Column = Grid.GetColumn(button);
-         buttonListPlayer1[Row, Column] = 1;
+      //   int[,] buttonListPlayer1 = new int[10, 10];
+      //   var Row = Grid.GetRow(button);
+      //   var Column = Grid.GetColumn(button);
+      //   buttonListPlayer1[Row, Column] = 1;
     }
 
    
